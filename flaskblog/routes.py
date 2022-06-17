8f05014971823766b501fa0d0d92254c914aa9ef
emmanuelcudjoe <emmanuel.cudjoe@turntabl.io>
2022-06-17 20:02:35 +0000

    Added pagination to the application

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,37 +1,19 @@
-from flask import flash, render_template, url_for, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from crypt import methods
+from flask import flash, render_template, url_for, redirect, request, abort
+from flaskblog.forms import RegistrationForm, LoginForm, PostForm
 from flaskblog.models import Post, User
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [ 
-    { 
-        "author": "Joe Regan",
-        "title": "Blog post 1",
-        "content": "First post content",
-        "date_posted": "June 13, 2022"
-    },
-    {
-        "author": "Sammy Johnson",
-        "title": "Blog post 2",
-        "content": "Second post content",
-        "date_posted": "June 12, 2022"
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog post 3",
-        "content": "Third post content",
-        "date_posted": "June 14, 2022"
-    }
-]
 
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    page = request.args.get("page", 1, type=int)
     app.logger.debug("Executing from the home route")
     app.logger.warning("A warning just occured")
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -82,3 +64,68 @@ def logout():
 @login_required
 def account():
     return render_template('account.html', title='Acount')
+
+
+@app.route("/post/new", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post has been created", "success")
+        return redirect(url_for("home"))
+    return render_template("create_post.html", 
+        title="New Post", 
+        legend="New Post",
+        form=form
+    )
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated", "success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", 
+        title="Update Post", 
+        legend="Update Post",
+        form=form)
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for("home"))
+
+
+@app.route("/user/<string:username>")
+def home(username):
+    page = request.args.get("page", 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
